9509da50a274b46542875649ec1dfadc9a6166ad
kingmanzhang <kingmanzhang@gmail.com>
2021-12-12 15:35:27 -0500

    rerun after propensity score matching

---
+++ src/main/sql/get_labs_for_colorectal_cancer_matched_control.sql
@@ -0,0 +1,4 @@
+select *
+from hai_az_prod.scc_and_epic_lab_after_loinc2hpo_2020july lab
+join matched_control_cohort using (medical_record_number) 
+where lab.loinc in ('6690-2', '789-8', '718-7', '4544-3', '787-2', '785-6', '786-4', '21000-5', '788-0', '777-3', '32207-3', '32623-1');
